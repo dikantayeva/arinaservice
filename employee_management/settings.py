579e5f4d13b8c6a8291aa6231bc59ec6bd680ff2
dikantayeva <63303303+dikantayeva@users.noreply.github.com>
2022-05-28 10:31:03 +0600

    Update settings.py

--- employee_management/settings.py
+++ employee_management/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
